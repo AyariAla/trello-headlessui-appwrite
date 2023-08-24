3455c1529352053fb144f1a3838c96dfa1bedba2
Ala <alaaeddine.ayari1@esprit.tn>
2023-08-24 16:17:12 +0200

    chore__

--- components/appwrite.ts
+++ components/appwrite.ts
@@ -2,7 +2,6 @@ import { Client, Account, ID, Databases, Storage } from 'appwrite';
 
 const client = new Client()
   .setEndpoint('https://cloud.appwrite.io/v1')
-  //   .setProject('64e4e8dd264e47b2cb5c');
   .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!);
 
 const account = new Account(client);
