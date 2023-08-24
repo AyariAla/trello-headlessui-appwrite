e1ffa56bd15e4b54f156e2b7a58021a4dd9c6282
Ala <alaaeddine.ayari1@esprit.tn>
2023-08-24 16:16:42 +0200

    feat: appWrite integration, added board component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Board from '@/components/Board';
 import Header from '@/components/Header';
 
 export default function Home() {
@@ -5,7 +6,7 @@ export default function Home() {
     <main>
       {/* Header */}
       <Header />
-      <h1>Trello 2.0 AI Clone</h1>
+      <Board />
     </main>
   );
 }
